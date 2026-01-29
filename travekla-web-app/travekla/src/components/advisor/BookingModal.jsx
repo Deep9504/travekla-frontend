7d50909b658c5ec17338cfa68f1119d3dd5b45b9
Deepshikha Singh <deepshikhasinghchas@gmail.com>
2026-01-29 19:46:28 +0530

    Created payment gateway

--- travekla-web-app/travekla/src/components/advisor/BookingModal.jsx
+++ travekla-web-app/travekla/src/components/advisor/BookingModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { Modal, Form, DatePicker, Input, Select, Button, message } from 'antd';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../../context/AuthContext';
 import axios from 'axios';
 
 const { Option } = Select;
